68ea6b9c7d585642d8cc238bdf20a971cc850067
GHARBI-R <62842493+GHARBI-R@users.noreply.github.com>
2021-12-14 13:14:40 +0100

    version finale

--- models/rdv.py
+++ models/rdv.py
@@ -13,12 +13,15 @@ class Rdv (models.Model):
 
     rdv_clt = fields.Many2one(comodel_name='client.fiche', string='Rendez_vous')
     rdv_raison_rel = fields.Char(string='Raison sociale', related='rdv_clt.raison')
+    email_clt_rel = fields.Char(string='Email principal ', related='rdv_clt.email_pri')
 
     motif = fields.Text( string='motif du rendez-vous', required=True)
     rdv_date_debut = fields.Datetime(string='Date Debut')
     rdv_date_fin = fields.Datetime(string='Date fin')
     client_id = fields.Many2one(comodel_name='client.fiche', string='Client', readonly=True)
     agent_id = fields.Many2one(comodel_name='agent.fiche', string='Agent')
+
+
 
     @api.model
     def create(self, vals):
